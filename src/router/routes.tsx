bf70fa8b460fea49f1a01738f94cfd094a1b5e42
riotDev17 <tomasalberto527@gmail.com>
2023-12-22 11:06:59 +0700

    chore: Add login route

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,12 +1,18 @@
 import { lazy } from 'react';
 
 const Index = lazy(() => import('../pages/admin/dashboard/Index'));
+const Login = lazy(() => import('../pages/admin/auth/SignIn'));
 
 const routes = [
-    {
-        path: '/',
-        element: <Index />
-    }
+  {
+    path: '/',
+    element: <Index />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+    layout: 'blank',
+  },
 ];
 
 export { routes };
