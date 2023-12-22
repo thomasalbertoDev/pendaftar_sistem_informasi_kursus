1c9f48b19b03ed3aaaac29ea6beebabd8d969f52
riotDev17 <tomasalberto527@gmail.com>
2023-12-22 21:14:01 +0700

    chore: Add css tippy

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import React, { Suspense } from 'react';
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
 
 // Perfect Scrollbar
 import 'react-perfect-scrollbar/dist/css/styles.css';
@@ -18,14 +18,15 @@ import router from './router/index';
 import { Provider } from 'react-redux';
 import store from './store/index';
 
+// Tippy
+import 'tippy.js/dist/tippy.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <React.StrictMode>
-        <Suspense>
-            <Provider store={store}>
-                <RouterProvider router={router} />
-            </Provider>
-        </Suspense>
-    </React.StrictMode>
+  <React.StrictMode>
+    <Suspense>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    </Suspense>
+  </React.StrictMode>
 );
-
