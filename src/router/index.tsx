f28c52b08ca7d0e94e151e075d619bca3805c2a8
riotDev17 <tomasalberto527@gmail.com>
2023-12-22 11:07:31 +0700

    refactor: tidy up the code

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -4,10 +4,10 @@ import DefaultLayout from '../Layouts/DefaultLayout';
 import { routes } from './routes';
 
 const finalRoutes = routes.map((route) => {
-    return {
-        ...route,
-        element: route.layout === 'blank' ? <BlankLayout>{route.element}</BlankLayout> : <DefaultLayout>{route.element}</DefaultLayout>,
-    };
+  return {
+    ...route,
+    element: route.layout === 'blank' ? <BlankLayout>{route.element}</BlankLayout> : <DefaultLayout>{route.element}</DefaultLayout>,
+  };
 });
 
 const router = createBrowserRouter(finalRoutes);
