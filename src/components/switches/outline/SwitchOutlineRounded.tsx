18eedff71e4fb7ad54428a7d26033b600bbd50e1
riotDev17 <tomasalberto527@gmail.com>
2023-12-28 21:02:23 +0700

    chore: change from value to checked input type checkbox

--- src/components/switches/outline/SwitchOutlineRounded.tsx
+++ src/components/switches/outline/SwitchOutlineRounded.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
 interface SwitchOutlineRoundedProps {
-  value?: boolean;
+  checked?: boolean;
   onChange?: React.ChangeEventHandler<HTMLInputElement>;
 }
 
-const SwitchOutlineRounded: React.FC<SwitchOutlineRoundedProps> = ({ value, onChange }) => {
+const SwitchOutlineRounded: React.FC<SwitchOutlineRoundedProps> = ({ checked, onChange }) => {
   return (
     <>
       <label className="w-12 h-6 relative">
-        <input id="custom_switch_checkbox1" type="checkbox" value={value ? 'true' : 'false'} className="custom_switch absolute w-full h-full opacity-0 z-10 cursor-pointer peer" onChange={onChange} />
+        <input id="custom_switch_checkbox1" type="checkbox" checked={checked} className="custom_switch absolute w-full h-full opacity-0 z-10 cursor-pointer peer" onChange={onChange} />
         <span className="outline_checkbox border-2 border-[#ebedf2] dark:border-white-dark block h-full rounded-full before:absolute before:left-1 before:bg-[#ebedf2] dark:before:bg-white-dark before:bottom-1 before:w-4 before:h-4 before:rounded-full peer-checked:before:left-7 peer-checked:border-primary peer-checked:before:bg-primary before:transition-all before:duration-300"></span>
       </label>
     </>
--- src/components/switches/solid/SwitchSolidDefault.tsx
+++ src/components/switches/solid/SwitchSolidDefault.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
 interface SwitchSolidDefaultProps {
-  value?: boolean;
+  checked?: boolean;
   onChange?: React.ChangeEventHandler<HTMLInputElement>;
 }
 
-const SwitchSolidDefault: React.FC<SwitchSolidDefaultProps> = ({ value, onChange }) => {
+const SwitchSolidDefault: React.FC<SwitchSolidDefaultProps> = ({ checked, onChange }) => {
   return (
     <>
       <label className="w-12 h-6 relative">
-        <input value={value ? 'true' : 'false'} type="checkbox" className="custom_switch absolute w-full h-full opacity-0 z-10 cursor-pointer peer" id="custom_switch_checkbox1" onChange={onChange} />
+        <input checked={checked} type="checkbox" className="custom_switch absolute w-full h-full opacity-0 z-10 cursor-pointer peer" id="custom_switch_checkbox1" onChange={onChange} />
         <span className="bg-[#ebedf2] dark:bg-dark block h-full before:absolute before:left-1 before:bg-white dark:before:bg-white-dark dark:peer-checked:before:bg-white before:bottom-1 before:w-4 before:h-4 peer-checked:before:left-7 peer-checked:bg-primary before:transition-all before:duration-300 "></span>
       </label>
     </>
