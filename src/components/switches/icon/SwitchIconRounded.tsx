ae32715b7aad2501d04dc7981cd807c8a4d11d46
riotDev17 <tomasalberto527@gmail.com>
2023-12-28 11:29:59 +0700

    components: Add components Switch

---
+++ src/components/switches/icon/SwitchIconRounded.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+interface SwitchIconRoundedProps {
+  value?: boolean;
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
+}
+
+const SwitchIconRounded: React.FC<SwitchIconRoundedProps> = ({ value, onChange }) => {
+  return (
+    <>
+      <label className="w-12 h-6 relative">
+        <input value={value ? 'true' : 'false'} type="checkbox" className="custom_switch absolute w-full h-full opacity-0 z-10 cursor-pointer peer" id="custom_switch_checkbox1" onChange={onChange} />
+        <span className="outline_checkbox bg-icon border-2 border-[#ebedf2] dark:border-white-dark block h-full rounded-full before:absolute before:left-1 before:bg-[#ebedf2] dark:before:bg-white-dark before:bottom-1 before:w-4 before:h-4 before:rounded-full before:bg-[url(/assets/images/close.svg)] before:bg-no-repeat before:bg-center peer-checked:before:left-7 peer-checked:before:bg-[url(/assets/images/checked.svg)] peer-checked:border-primary peer-checked:before:bg-primary before:transition-all before:duration-300"></span>
+      </label>
+    </>
+  );
+};
+
+export default SwitchIconRounded;
---
+++ src/components/switches/solid/SwitchSolidRounded.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+interface SwitchSolidRoundedProps {
+  value?: boolean;
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
+}
+
+const SwitchSolidRounded: React.FC<SwitchSolidRoundedProps> = ({ value, onChange }) => {
+  return (
+    <>
+      <label className="w-12 h-6 relative">
+        <input value={value ? 'true' : 'false'} type="checkbox" className="custom_switch absolute w-full h-full opacity-0 z-10 cursor-pointer peer" id="custom_switch_checkbox1" onChange={onChange} />
+        <span className="bg-[#ebedf2] dark:bg-dark block h-full rounded-full before:absolute before:left-1 before:bg-white dark:before:bg-white-dark dark:peer-checked:before:bg-white before:bottom-1 before:w-4 before:h-4 before:rounded-full peer-checked:before:left-7 peer-checked:bg-primary before:transition-all before:duration-300"></span>
+      </label>
+    </>
+  );
+};
+
+export default SwitchSolidRounded;
