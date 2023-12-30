0095190816010caa235ab412b607bd6f67d28a44
riotDev17 <tomasalberto527@gmail.com>
2023-12-30 11:22:11 +0700

    dashboard

--- src/pages/admin/dashboard/Index.tsx
+++ src/pages/admin/dashboard/Index.tsx
@@ -1,7 +1,19 @@
+import TableBasic from '../../../components/tables/TableBasic';
+
 const Index = () => {
-    return (
-        <h1>Hello world</h1>
-    );
+  // const columns = [
+  //   { accessor: 'id', title: 'ID' },
+  //   { accessor: 'firstName', title: 'First Name' },
+  //   { accessor: 'lastName', title: 'Last Name' },
+  //   { accessor: 'email', title: 'Email' },
+  //   { accessor: 'phone', title: 'Phone' },
+  // ];
+
+  return (
+    <>
+      <h1>Hello World</h1>
+    </>
+  );
 };
 
 export default Index;
