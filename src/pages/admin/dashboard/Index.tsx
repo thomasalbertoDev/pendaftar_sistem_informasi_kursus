4d31debe4c2faf7dd5375bd4638d93d38b503475
riotDev17 <tomasalberto527@gmail.com>
2023-12-30 12:34:11 +0700

    index

--- src/pages/admin/dashboard/Index.tsx
+++ src/pages/admin/dashboard/Index.tsx
@@ -1,5 +1,8 @@
 import BreadcrumbsBasic from '../../../components/breadcrumbs/BreadcrumbsBasic';
 import BreadcrumbsDefault from '../../../components/breadcrumbs/BreadcrumbsDefault';
+import SearchBox from '../../../components/searchs/SearchBox';
+import SearchOverlay from '../../../components/searchs/SearchOverlay';
+import SearchRounded from '../../../components/searchs/SearchRounded';
 import TableBasic from '../../../components/tables/TableBasic';
 
 const Index = () => {
@@ -11,17 +14,20 @@ const Index = () => {
   //   { accessor: 'phone', title: 'Phone' },
   // ];
 
-  const menus = [
-    { label: 'Home', link: '/' },
-    { label: 'Category', link: '/category' },
-    { label: 'Subcategory', link: '/category/subcategory' },
-    // Tambahkan menu tambahan jika diperlukan
-  ];
+  // const menus = [
+  //   { label: 'Home', link: '/' },
+  //   { label: 'Category', link: '/category' },
+  //   { label: 'Subcategory', link: '/category/subcategory' },
+  //   // Tambahkan menu tambahan jika diperlukan
+  // ];
 
   return (
     <>
       {/* <h1>Hello World</h1> */}
-      <BreadcrumbsBasic menus={menus} />
+      {/* <BreadcrumbsBasic menus={menus} /> */}
+      {/* <SearchRounded /> */}
+      {/* <SearchBox /> */}
+      {/* <SearchOverlay /> */}
     </>
   );
 };
