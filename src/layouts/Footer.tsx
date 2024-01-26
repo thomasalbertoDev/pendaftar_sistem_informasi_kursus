c4e66f31bfa11498b383947e63bb0823e286a109
riotDev17 <tomasalberto527@gmail.com>
2024-01-26 11:55:05 +0700

    chore: mengganti nama footer

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,5 +1,5 @@
 const Footer = () => {
-    return <div className="dark:text-white-dark text-center ltr:sm:text-left rtl:sm:text-right p-6 pt-0 mt-auto">© {new Date().getFullYear()}. Vristo All rights reserved.</div>;
+  return <div className="dark:text-white-dark text-center ltr:sm:text-left rtl:sm:text-right p-6 pt-0 mt-auto">© {new Date().getFullYear()}. GudangNet rights reserved.</div>;
 };
 
 export default Footer;
