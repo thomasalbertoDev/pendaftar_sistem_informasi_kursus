b4ce1dec915b60af400af21c56ae192090b1095b
riotDev17 <tomasalberto527@gmail.com>
2024-01-27 00:28:36 +0700

    chore: menambahkan ProtectedRoute

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,20 @@
-import { routes } from './routes';
-import { createBrowserRouter } from 'react-router-dom';
 import BlankLayout from '../layouts/BlankLayout';
 import DefaultLayout from '../layouts/DefaultLayout';
+import ProtectedRoute from '../configs/ProtectedRoute';
+import { routes } from './routes';
+import { createBrowserRouter } from 'react-router-dom';
 
 const finalRoutes = routes.map((route) => {
   return {
     ...route,
-    element: route.layout === 'blank' ? <BlankLayout>{route.element}</BlankLayout> : <DefaultLayout>{route.element}</DefaultLayout>,
+    element:
+      route.layout === 'blank' ? (
+        <BlankLayout>{route.element}</BlankLayout>
+      ) : (
+        <ProtectedRoute>
+          <DefaultLayout>{route.element}</DefaultLayout>
+        </ProtectedRoute>
+      ),
   };
 });
 
