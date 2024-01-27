b3c225db612e3d5c646564c8aefe00c87c1c7945
riotDev17 <tomasalberto527@gmail.com>
2024-01-27 11:21:05 +0700

    chore: setup page 404

--- src/pages/Error404.tsx
+++ src/pages/Error404.tsx
@@ -1,8 +1,8 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { setPageTitle } from '../store/themeConfigSlice';
+import { useEffect } from 'react';
 import { IRootState } from '../store';
+import { setPageTitle } from '../store/themeConfigSlice';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Error404 = () => {
   const dispatch = useDispatch();
@@ -16,9 +16,9 @@ const Error404 = () => {
       <div className="px-6 py-16 text-center font-semibold before:container before:absolute before:left-1/2 before:-translate-x-1/2 before:rounded-full before:bg-[linear-gradient(180deg,#4361EE_0%,rgba(67,97,238,0)_50.73%)] before:aspect-square before:opacity-10 md:py-20">
         <div className="relative">
           <img src={isDark ? '/assets/images/error/404-dark.svg' : '/assets/images/error/404-light.svg'} alt="404" className="mx-auto -mt-10 w-full max-w-xs object-cover md:-mt-14 md:max-w-xl" />
-          <p className="mt-5 text-base dark:text-white">The page you requested was not found!</p>
-          <Link to="/" className="btn btn-gradient mx-auto !mt-7 w-max border-0 uppercase shadow-none">
-            Home
+          <p className="mt-5 text-base dark:text-white">Halaman Yang Anda Cari Tidak Dapat Ditemukan , Silahkan Kembali Ke Beranda!</p>
+          <Link to="/" className="btn btn-primary mx-auto !mt-7 w-max border-0 uppercase shadow-none py-3 hover:opacity-75">
+            Kembali Ke Beranda
           </Link>
         </div>
       </div>
--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -3,6 +3,7 @@ import { lazy } from 'react';
 const Index = lazy(() => import('../pages/admin/dashboard/Index'));
 const Login = lazy(() => import('../pages/admin/auth/SignIn'));
 const Register = lazy(() => import('../pages/admin/auth/SignUp'));
+const Error404 = lazy(() => import('../pages/Error404'));
 
 const routes = [
   {
@@ -17,6 +18,11 @@ const routes = [
   {
     path: '/register',
     element: <Register />,
+    layout: 'blank',
+  },
+  {
+    path: '*',
+    element: <Error404 />,
     layout: 'blank',
   },
 ];
