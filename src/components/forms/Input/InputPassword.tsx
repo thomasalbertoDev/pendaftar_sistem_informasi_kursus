1633f58a2d77997fcd12e213ad5fc97d37170842
riotDev17 <tomasalberto527@gmail.com>
2024-02-13 09:46:24 +0700

    feat: Fitur hide dan show password

--- src/components/forms/Input/InputPassword.tsx
+++ src/components/forms/Input/InputPassword.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import IconEye from '../../Icons/IconEye';
+import { Icon } from '@iconify/react';
 
 interface InputPasswordProps {
   id: string;
@@ -12,13 +14,37 @@ interface InputPasswordProps {
 }
 
 const InputPassword: React.FC<InputPasswordProps> = ({ id, name, value, onChange, placeholder, label, error, isInputFilled }) => {
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const inputType = showPassword ? 'text' : 'password';
   const isFilled = value !== '';
 
   return (
     <>
       <div className="mb-5">
         <label htmlFor={label}>{label}</label>
-        <input id={id} type="password" name={name} value={value} onChange={onChange} placeholder={placeholder} className={`form-input ${error ? 'error' : ''} py-3 rounded-lg`} />
+        <div className="relative">
+          <input
+            id={id}
+            type={inputType} // Use dynamic input type
+            name={name}
+            value={value}
+            onChange={onChange}
+            placeholder={placeholder}
+            className={`form-input ${error ? 'error' : ''} py-3  rounded-lg`} // Add padding-left to accommodate the icon
+          />
+          <button type="button" className="absolute inset-y-0 right-0 px-3 py-2" onClick={toggleShowPassword}>
+            {showPassword ? (
+              <Icon icon="majesticons:eye-off" width="1.3rem" height="1.3rem" className="text-primary" />
+            ) : (
+              <Icon icon="mdi:eye" width="1.3rem" height="1.3rem" className="text-primary" />
+            )}
+          </button>
+        </div>
 
         {error && <span className="text-danger">{error}</span>}
         {isFilled && !error && <span className="text-success">{isInputFilled}</span>}
