2d17eef1bd5bdb183da93ff430fcf61d0f73d6f8
riotDev17 <tomasalberto527@gmail.com>
2024-02-14 21:45:05 +0700

    chore: mengganti Form import

--- src/pages/agama/Form/layouts/FormLayouts.tsx
+++ src/pages/agama/Form/layouts/FormLayouts.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form } from 'react-router-dom';
+import { Form } from 'formik';
 import InputText from '../../../../components/forms/Input/InputText';
 
 interface FormLayoutsProps {
