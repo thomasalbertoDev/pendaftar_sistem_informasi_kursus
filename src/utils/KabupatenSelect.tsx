da0f14e2ffbc7108359d3f684554260dc6020bf0
riotDev17 <tomasalberto527@gmail.com>
2024-02-15 12:06:09 +0700

    feat: menambahkan komponent provinsi , kabupaten , kecamatan , kelurahan

---
+++ src/utils/KabupatenSelect.tsx
@@ -0,0 +1,81 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import SelectSearch from '../components/forms/Select/SelectSearch';
+
+interface KabupatenSelectProps {
+  id: string;
+  name: string;
+  label: string;
+  value: any;
+  error: string;
+  onChange: (value: any) => void;
+  isInputFilled: string;
+  placeholder: string;
+  provinsiId: string;
+  onKabupatenChange: (value: any) => void;
+}
+
+const KabupatenSelect: React.FC<KabupatenSelectProps & { provinsiId: string }> = ({ id, name, label, error, value, onChange, isInputFilled, placeholder, provinsiId, onKabupatenChange }) => {
+  const isFilled = value !== '';
+  const [kabupaten, setKabupaten] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchKabupaten = async () => {
+      try {
+        const response = await axios.get(`https://www.emsifa.com/api-wilayah-indonesia/api/regencies/${provinsiId}.json`);
+        const transformedKabupaten = response?.data.map((item: any) => ({
+          value: item.id,
+          label: item.name,
+        }));
+
+        setKabupaten(transformedKabupaten);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchKabupaten();
+  }, [provinsiId]);
+
+  const handleKabupatenChange = (selectedKabupatenValue: any) => {
+    onChange(selectedKabupatenValue?.value);
+    onKabupatenChange(selectedKabupatenValue?.value);
+  };
+
+  const styles = {
+    control: (baseStyles: any) => ({
+      ...baseStyles,
+      border: '1px solid #e5e7eb',
+      borderRadius: '0.5rem',
+      paddingBottom: '6px',
+      paddingTop: '6px',
+      paddingLeft: '8px',
+      color: '#1f2937',
+      backgroundColor: '#fff',
+    }),
+  };
+
+  return (
+    <>
+      <div className="mb-5">
+        <SelectSearch
+          id={id}
+          name={name}
+          label={label}
+          value={value}
+          error={error}
+          styles={styles}
+          options={kabupaten}
+          onChange={handleKabupatenChange}
+          placeholder={placeholder}
+          isInputFilled={isInputFilled}
+        />
+
+        {error && <span className="text-danger">{error}</span>}
+        {isFilled && !error && <span className="text-success">{isInputFilled}</span>}
+      </div>
+    </>
+  );
+};
+
+export default KabupatenSelect;
---
+++ src/utils/KelurahanSelect.tsx
@@ -0,0 +1,65 @@
+import axios from 'axios';
+import { use } from 'i18next';
+import { useEffect, useState } from 'react';
+import SelectSearch from '../components/forms/Select/SelectSearch';
+
+interface KelurahanSelectProps {
+  id: string;
+  name: string;
+  label: string;
+  value: any;
+  error: string;
+  onChange: (value: any) => void;
+  isInputFilled: string;
+  placeholder: string;
+  kecamatanId: string;
+}
+
+const KelurahanSelect: React.FC<KelurahanSelectProps & { kecamatanId: string }> = ({ id, name, label, error, value, onChange, isInputFilled, placeholder, kecamatanId }) => {
+  const isFilled = value !== '';
+  const [kelurahan, setKelurahan] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchKelurahan = async () => {
+      try {
+        const response = await axios.get(`https://www.emsifa.com/api-wilayah-indonesia/api/villages/${kecamatanId}.json`);
+        const transformedKelurahan = response?.data.map((item: any) => ({
+          value: item.id,
+          label: item.name,
+        }));
+
+        setKelurahan(transformedKelurahan);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchKelurahan();
+  }, [kecamatanId]);
+
+  const styles = {
+    control: (baseStyles: any) => ({
+      ...baseStyles,
+      border: '1px solid #e5e7eb',
+      borderRadius: '0.5rem',
+      paddingBottom: '6px',
+      paddingTop: '6px',
+      paddingLeft: '8px',
+      color: '#1f2937',
+      backgroundColor: '#fff',
+    }),
+  };
+
+  return (
+    <>
+      <div className="mb-5">
+        <SelectSearch id={id} name={name} label={label} value={value} error={error} styles={styles} options={kelurahan} onChange={onChange} placeholder={placeholder} isInputFilled={isInputFilled} />
+
+        {error && <span className="text-danger">{error}</span>}
+        {isFilled && !error && <span className="text-success">{isInputFilled}</span>}
+      </div>
+    </>
+  );
+};
+
+export default KelurahanSelect;
