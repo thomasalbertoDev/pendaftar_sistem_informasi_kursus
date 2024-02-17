265dfe7eb07ee848b5d747975ff3335cf96c0398
riotDev17 <tomasalberto527@gmail.com>
2024-02-17 11:55:22 +0700

    textarea

--- src/components/forms/Input/InputTexarea.tsx
+++ src/components/forms/Input/InputTexarea.tsx
@@ -19,7 +19,7 @@ const InputTextarea: React.FC<InputTextareaProps> = ({ id, name, rows, value, on
     <>
       <div className="mb-5">
         <label htmlFor={label}>{label}</label>
-        <textarea id={id} name={name} rows={rows} value={value} onChange={onChange} placeholder={placeholder} className={`form-textarea ${error ? 'error' : ''} mb-1`} />
+        <textarea id={id} name={name} rows={rows} value={value} onChange={onChange} placeholder={placeholder} className={`form-textarea ${error ? 'error' : ''} mb-1 rounded-lg`} />
 
         {error && <span className="text-danger">{error}</span>}
         {isFilled && !error && <span className="text-success">{isInputFilled}</span>}
