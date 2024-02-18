399c0d03d48d1a5df775f0cfafb8b0e4cb261ac2
riotDev17 <tomasalberto527@gmail.com>
2024-02-18 20:47:03 +0700

    format jam

--- src/helpers/FormatJam.tsx
+++ src/helpers/FormatJam.tsx
@@ -1,3 +1,10 @@
-export default function FormatJam(jam?: string): string | undefined {
-  return jam?.substr(0, 5);
+export default function FormatJam(jam: string | undefined): string {
+  if (!jam) return '';
+
+  const waktu: Date = new Date(jam);
+
+  const jamFormatted: string = waktu.getHours().toString().padStart(2, '0');
+  const menitFormatted: string = waktu.getMinutes().toString().padStart(2, '0');
+
+  return `${jamFormatted}:${menitFormatted}`;
 }
