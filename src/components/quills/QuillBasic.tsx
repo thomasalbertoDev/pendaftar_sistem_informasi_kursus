a079b4c450f8d057f15b1f2202ba6e2c4e7ab551
riotDev17 <tomasalberto527@gmail.com>
2024-02-18 11:13:42 +0700

    quill

--- src/components/quills/QuillBasic.tsx
+++ src/components/quills/QuillBasic.tsx
@@ -8,15 +8,16 @@ interface QuillBasicProps {
   onChange: (content: string) => void;
   error: string;
   isInputFilled: string;
+  placeholder?: string;
 }
 
-const QuillBasic: React.FC<QuillBasicProps> = ({ id, label, value, onChange, error, isInputFilled }) => {
+const QuillBasic: React.FC<QuillBasicProps> = ({ id, label, value, onChange, error, isInputFilled, placeholder }) => {
   const isFilled = value !== '';
 
   return (
     <div>
       <label htmlFor={id}>{label}</label>
-      <ReactQuill theme="snow" id={id} value={value} onChange={onChange} className="mb-1" />
+      <ReactQuill theme="snow" id={id} value={value} onChange={onChange} className="mb-1" placeholder={placeholder} />
 
       {error && <span className="text-danger">{error}</span>}
       {isFilled && !error && <div className="text-success" dangerouslySetInnerHTML={{ __html: isInputFilled }} />}
