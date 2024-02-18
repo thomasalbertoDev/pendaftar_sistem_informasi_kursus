77258917faf988dedbaf3e5046d977008e2cfceb
riotDev17 <tomasalberto527@gmail.com>
2024-02-18 21:01:02 +0700

    date time components

--- src/components/forms/date/DateTime.tsx
+++ src/components/forms/date/DateTime.tsx
@@ -3,20 +3,27 @@ import { IRootState } from '../../../store';
 import { useSelector } from 'react-redux';
 
 interface DateTimeProps {
+  id: string;
+  name: string;
   label: string;
   value: Date;
   placeholder?: string;
+  error: string;
   onChange: (selectedDates: Date[], dateStr: string) => void;
+  isInputFilled: string;
 }
 
-const DateTime: React.FC<DateTimeProps> = ({ label, value, placeholder, onChange }) => {
+const DateTime: React.FC<DateTimeProps> = ({ id, name, label, value, placeholder, error, onChange, isInputFilled }) => {
   const isRtl = useSelector((state: IRootState) => state.themeConfig.rtlClass) === 'rtl' ? true : false;
+  const isFilled = value !== null && value.toString().trim() !== '';
 
   return (
     <>
       <div className="mb-5">
         <label htmlFor={label}>{label}</label>
         <Flatpickr
+          id={id}
+          name={name}
           data-enable-time
           options={{
             enableTime: true,
@@ -24,10 +31,13 @@ const DateTime: React.FC<DateTimeProps> = ({ label, value, placeholder, onChange
             position: isRtl ? 'auto right' : 'auto left',
           }}
           value={value}
-          className="form-input"
+          className="form-input dark:bg-white mb-1 dark:text-black py-3 rounded-lg  "
           onChange={onChange}
           placeholder={placeholder}
         />
+
+        {error && <span className="text-danger">{error}</span>}
+        {isFilled && !error && <span className="text-success">{isInputFilled}</span>}
       </div>
     </>
   );
--- src/components/forms/date/PreloadingTime.tsx
+++ src/components/forms/date/PreloadingTime.tsx
@@ -3,20 +3,28 @@ import { IRootState } from '../../../store';
 import { useSelector } from 'react-redux';
 
 interface PreloadingTimeProps {
+  id: string;
+  name: string;
   label: string;
-  value: Date;
-  placeholder?: string;
+  value: Date | string;
+  placeholder: string;
+  error: string;
   onChange: (selectedDates: Date[], dateStr: string) => void;
+  isInputFilled: string;
 }
 
-const PreloadingTime: React.FC<PreloadingTimeProps> = ({ label, value, placeholder, onChange }) => {
+const PreloadingTime: React.FC<PreloadingTimeProps> = ({ id, name, label, value, placeholder, error, onChange, isInputFilled }) => {
   const isRtl = useSelector((state: IRootState) => state.themeConfig.rtlClass) === 'rtl' ? true : false;
+
+  const isFilled = value !== null && value.toString().trim() !== '';
 
   return (
     <>
       <div className="mb-5">
         <label htmlFor={label}>{label}</label>
         <Flatpickr
+          id={id}
+          name={name}
           options={{
             noCalendar: true,
             enableTime: true,
@@ -24,10 +32,13 @@ const PreloadingTime: React.FC<PreloadingTimeProps> = ({ label, value, placehold
             position: isRtl ? 'auto right' : 'auto left',
           }}
           value={value}
-          className="form-input"
+          className="form-input dark:bg-white mb-1 dark:text-black py-3 rounded-lg  "
           onChange={onChange}
           placeholder={placeholder}
         />
+
+        {error && <span className="text-danger">{error}</span>}
+        {isFilled && !error && <span className="text-success">{isInputFilled}</span>}
       </div>
     </>
   );
