af08bfcd3c312803b0f1ca75d096c0cdf7211949
riotDev17 <tomasalberto527@gmail.com>
2024-02-19 20:07:11 +0700

    interface

--- src/api/auth/services/requestLogin.ts
+++ src/api/auth/services/requestLogin.ts
@@ -2,6 +2,14 @@ import { login } from '../api';
 import auth from '../../../configs/auth';
 import ShowToast from '../../../helpers/ShowToast';
 
+interface LoginResponse {
+  status: number;
+  token: string;
+  username: string;
+  nama: string;
+  role: string;
+}
+
 const handleSuccess = (): boolean => {
   ShowToast('success', 'Selamat Datang!');
   return true;
@@ -11,13 +19,6 @@ const handleError = (): boolean => {
   ShowToast('error', 'Username Atau Password Salah!');
   return false;
 };
-
-interface LoginResponse {
-  token: string;
-  username: string;
-  nama: string;
-  role: string;
-}
 
 export const requestLogin = async (username: string, password: string): Promise<boolean | undefined> => {
   try {
