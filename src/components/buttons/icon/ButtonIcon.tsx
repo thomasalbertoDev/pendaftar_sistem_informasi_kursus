3cc3d79a6b1d7f5830e4d11e56676b58b9dcb2ee
riotDev17 <tomasalberto527@gmail.com>
2024-02-26 19:48:05 +0700

    button icon

--- src/components/buttons/icon/ButtonIcon.tsx
+++ src/components/buttons/icon/ButtonIcon.tsx
@@ -5,12 +5,13 @@ interface ButtonIconProps {
   icon: string;
   onClick?: () => void;
   backgroundColor?: string;
+  disabled?: boolean;
 }
 
-const ButtonIcon = forwardRef(({ icon, onClick, backgroundColor }: ButtonIconProps, ref: ForwardedRef<HTMLButtonElement>) => {
+const ButtonIcon = forwardRef(({ icon, onClick, backgroundColor, disabled }: ButtonIconProps, ref: ForwardedRef<HTMLButtonElement>) => {
   return (
     <>
-      <button ref={ref} onClick={onClick} type="button" className={`btn ${backgroundColor} `}>
+      <button ref={ref} onClick={onClick} type="button" className={`btn ${backgroundColor} `} disabled={disabled}>
         <Icon icon={icon} width={17} />
       </button>
     </>
