7ede4a9a71904dddccc34231bbc67a67ac0b8833
riotDev17 <tomasalberto527@gmail.com>
2024-02-29 20:34:04 +0700

    alert

--- src/components/alerts/solid/AlertSolidSuccess.tsx
+++ src/components/alerts/solid/AlertSolidSuccess.tsx
@@ -4,21 +4,19 @@ import IconX from '../../Icons/IconX';
 interface AlertSolidSuccessProps {
   title: string;
   text: string;
-  onClick?: () => void;
 }
 
-const AlertSolidSuccess: React.FC<AlertSolidSuccessProps> = ({ title, text, onClick }) => {
+const AlertSolidSuccess: React.FC<AlertSolidSuccessProps> = ({ title, text }) => {
   return (
     <>
-      <div className="flex items-center border p-3.5 rounded text-white bg-success">
+      <div className="flex items-center border p-3.5 text-white bg-success rounded-lg">
         <span className="ltr:pr-2 rtl:pl-2">
           <strong className="ltr:mr-1 rtl:ml-1">{title}</strong>
           {text}
         </span>
-        <button type="button" className="ltr:ml-auto rtl:mr-auto hover:opacity-80" onClick={onClick}>
-          <IconX className="w-5 h-5" />
-        </button>
       </div>
     </>
   );
 };
+
+export default AlertSolidSuccess;
