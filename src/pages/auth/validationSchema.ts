97720ed8a1536cdf1109bb8a93d5af5e36f399be
riotDev17 <tomasalberto527@gmail.com>
2024-03-02 10:56:48 +0700

    auth validation

--- src/pages/auth/validationSchema.ts
+++ src/pages/auth/validationSchema.ts
@@ -2,6 +2,7 @@ import * as yup from 'yup';
 
 export const validationSchema = yup.object().shape({
   otp: yup.string().required('OTP Wajib Diisi'),
+  nama: yup.string().required('Nama Wajib Diisi'),
   email: yup.string().email('Email Tidak Valid').required('Email Wajib Diisi'),
   username: yup.string().required('Username Wajib Diisi'),
   password: yup.string().required('Password Wajib Diisi'),
